5c1e11494495a67d47dcf7a9a7374dea55c9e183
Rostislav <r_krivenko@kbtu.kz>
2026-04-07 20:16:16 +0500

    upd

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('GameTracker');
+  protected readonly title = signal('Frontend');
 }
