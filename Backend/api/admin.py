f45cab319d6b1663944785ff1fc8f76a024b8f8d
Rostislav <r_krivenko@kbtu.kz>
2026-04-12 16:04:36 +0500

    Register models in admin.py

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Game, Review, Library, Profile
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Review)
+admin.site.register(Library)
+admin.site.register(Profile)
