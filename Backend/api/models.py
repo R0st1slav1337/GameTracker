73c8ef2403f02eedf9dbfc770881ec9f27ca9003
Rostislav <r_krivenko@kbtu.kz>
2026-04-14 15:31:43 +0500

    Refactor Game model: remove 'genre' field and add 'genres', 'rating', and 'slug' fields

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -3,11 +3,15 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Game(models.Model):
+    rawg_id = models.IntegerField(unique=True, null=True, blank=True)
     title = models.CharField(max_length = 255)
     description = models.TextField()
-    genre = models.CharField(max_length=100, blank=True)
     release_date = models.DateField(null = True, blank=True)
+
+    rating = models.FloatField(null=True, blank=True)
+    genres = models.CharField(max_length=255, blank=True)
     image = models.URLField(blank=True)
+    slug = models.SlugField(max_length=255, blank=True)
 
     def __str__(self):
         return self.title
