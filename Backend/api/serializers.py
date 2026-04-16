0afda98f74a5faae1d387ff4cbcb7d6c5c560e46
Flyli0 <kirillrozkov653@gmail.com>
2026-04-16 21:53:11 +0500

    changed serializer for Library

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Game, Review, Library, Profile
 from django.contrib.auth.models import User
+from .rawg_service import get_rawg_game
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
@@ -42,3 +43,26 @@ class LibrarySerializer(serializers.ModelSerializer):
         model = Library
         fields = '__all__'
         read_only_fields = ['user']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        rawg_id = validated_data.pop('rawg_id')
+        game = Game.objects.filter(rawg_id=rawg_id)
+
+        if not game:
+            data = get_rawg_game(rawg_id)
+            game = Game.objects.create(
+                rawg_id = data["rawg_id"],
+                title = data["title"],
+                description = data.get("description",""),
+                release_date = data["release_date"],
+                rating = data["rating"],
+                generes = data["generes"],
+                image = data.get("image"),
+                slug = data["slug"],
+            )
+        return Library.objects.create(
+            user = user,
+            game = game,
+            **validated_data
+        )
