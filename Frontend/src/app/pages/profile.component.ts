6b50c9c9a6b4496f73b06c276cb34a55d8e2eff1
AruzhanNurkhassimova <a_nurkhassimova@kbtu.kz>
2026-04-19 03:54:08 +0500

    Implement profile page displaying user data

--- Frontend/src/app/pages/profile.component.ts
+++ Frontend/src/app/pages/profile.component.ts
@@ -6,11 +6,28 @@ import { ApiService } from '../../services/api.service';
   standalone: true,
   template: `
     <h2>Profile</h2>
+
+    <p><b>Username:</b> {{ profile?.user }} </p>
+    <p><b>Bio:</b> {{ profile?.bio }} </p>
+
+    <button (click)="load()">Refresh</button>
   `
 })
 
 export class ProfileComponent {
+  profile: any;
+
   constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.api.getProfile().subscribe((data: any)=> {
+      this.profile = data;
+    });
+  }
 
   logout() {
     const refresh = localStorage.getItem('refresh');
