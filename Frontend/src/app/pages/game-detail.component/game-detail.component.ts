decaaff76695840cfd0586edbf8dff41b6a9e49a
Flyli0 <kirillrozkov653@gmail.com>
2026-04-18 20:39:09 +0500

    added loading and textCleaner function (it cleans text from html tags)

--- Frontend/src/app/pages/game-detail.component/game-detail.component.ts
+++ Frontend/src/app/pages/game-detail.component/game-detail.component.ts
@@ -13,21 +13,29 @@ export class GameDetailComponent {
   route = inject(ActivatedRoute);
   gameService = inject(GameService);
   game_details = signal<any>("")
+  loading = signal<boolean>(false)
 
 
   ngOnInit(){
+    this.loading.set(true);
     this.route.paramMap.subscribe(params => {
       console.log(params);
       const id: number = Number(params.get('id'))
       console.log(id);
       this.gameService.getGameDetail(id).subscribe({
         next: (res) => {
+          res.description = this.textCleaner(res.description);
           this.game_details.set(res)
           console.log(this.game_details);
+          this.loading.set(false);
         }
       })
     })
   }
 
+  textCleaner(html: string){
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || '';
+  }
 
 }
