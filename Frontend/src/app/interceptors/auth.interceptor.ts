aa38fca22807845054c007166a358defccbe17ec
Flyli0 <kirillrozkov653@gmail.com>
2026-04-20 22:48:34 +0500

    interceptor now processes Unauthorized responses

--- Frontend/src/app/interceptors/auth.interceptor.ts
+++ Frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,7 +1,13 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpClient, HttpInterceptorFn} from '@angular/common/http';
+import {catchError, switchMap, throwError} from 'rxjs';
+import {Router} from '@angular/router';
+import {inject} from '@angular/core';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access');
+  const router = inject(Router);
+  const http = inject(HttpClient);
+  let authReq = req;
   console.log("INTERCEPT:", req.url);
 
   if (req.url.includes('auth/')) {
@@ -18,5 +24,38 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     return next(cloned);
   }
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError(err => {
+      if (err.status === 401) {
+        const refresh = localStorage.getItem('refresh');
+
+        if (!refresh) {
+          router.navigate(['/login']);
+          return throwError(() => err);
+        }
+
+        return http.post('/api/auth/refresh/', { refresh }).pipe(
+          switchMap((res: any) => {
+            localStorage.setItem('access', res.access);
+
+            const newReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${res.access}`
+              }
+            });
+
+            return next(newReq);
+          }),
+          catchError(() => {
+
+            localStorage.clear();
+            router.navigate(['/login']);
+            return throwError(() => err);
+          })
+        );
+      }
+
+      return throwError(() => err);
+    })
+  );
 }
