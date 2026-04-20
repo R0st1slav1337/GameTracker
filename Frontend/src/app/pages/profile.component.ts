e54afb59722ddf8c77a35b07bff0c940710907c5
AruzhanNurkhassimova <a_nurkhassimova@kbtu.kz>
2026-04-20 10:32:46 +0500

    Add display user library in profile page

--- Frontend/src/app/pages/profile.component.ts
+++ Frontend/src/app/pages/profile.component.ts
@@ -7,26 +7,42 @@ import { ApiService } from '../../services/api.service';
   template: `
     <h2>Profile</h2>
 
-    <p><b>Username:</b> {{ profile?.user }} </p>
+    <p><b>Username:</b> {{ profile?.username }} </p>
     <p><b>Bio:</b> {{ profile?.bio }} </p>
 
     <button (click)="load()">Refresh</button>
+    <button (click)="logout()">Logout</button>
+
+    <h3>My Library</h3>
+    <ul>
+      @for (item of library; track item.id) {
+        <li>{{ item.game_title }} - {{ item.status }}</li>
+      }
+    </ul>
   `
 })
 
 export class ProfileComponent {
   profile: any;
+  library: any[] = [];
 
   constructor(private api: ApiService) {}
 
   ngOnInit() {
     this.load();
+    this.loadLibrary();
   }
 
   load() {
     this.api.getProfile().subscribe((data: any)=> {
       this.profile = data;
     });
+  }
+
+  loadLibrary() {
+    this.api.getLibrary().subscribe((data: any)=> {
+      this.library = data;
+    })
   }
 
   logout() {
