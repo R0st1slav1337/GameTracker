30efa351f2d51d369c559e8f76de8360e3bd7f25
Rostislav <r_krivenko@kbtu.kz>
2026-04-21 12:42:00 +0500

    make correct logic for logout and login

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { GamesComponent } from './pages/games/games.component';
-import { ProfileComponent } from './pages/profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { GameDetailComponent } from './pages/game-detail.component/game-detail.component';
 import { PublicProfileComponent } from './pages/public-profile.component';
